e579fd03c85ebca76589aac7d39713ff7a813a63
Umesh Rane <umesh.rane@rightlife.com>
2025-12-30 13:02:09 +0530

    Issues resolved 1.  Resolved Navigation issue From Notification To Practise Affirmation Page. 2. Update the Analytics Events for HOME_PAGE_FIRST_OPEN

--- app/src/main/java/com/jetsynthesys/rightlife/ui/affirmation/NotificationHelper.kt
+++ app/src/main/java/com/jetsynthesys/rightlife/ui/affirmation/NotificationHelper.kt
@@ -33,6 +33,7 @@ object NotificationHelper {
         else
             Intent(context, PractiseAffirmationPlaylistActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                putExtra("From_Notification",true)
             }
 
         val pendingIntent = PendingIntent.getActivity(
