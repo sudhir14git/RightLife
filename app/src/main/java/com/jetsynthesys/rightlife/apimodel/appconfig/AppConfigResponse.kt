3200ce90c2365b236845d66e5dfd3ae211d5cfa2
Umesh Rane <umesh.rane@rightlife.com>
2026-01-02 12:42:48 +0530

    New Feature- Challenge - Challenge Info Page Design - Added challenge info page into bottom sheet dialog - Register challenge card design - API implementation for register challenge card for getting data from backend - API implementation for join the challenge - Unlock challenge card design - binding data to unlock card from API response - New Flaoting Loader class added to show the Loader for background tasks. - AppConfig API changes as per new parameter added "isChallengeStart" for the Challenge. - Added AppConfig SharedPreference Name in not to clear to avoid the crash

--- app/src/main/java/com/jetsynthesys/rightlife/apimodel/appconfig/AppConfigResponse.kt
+++ app/src/main/java/com/jetsynthesys/rightlife/apimodel/appconfig/AppConfigResponse.kt
@@ -1,58 +1,59 @@
 package com.jetsynthesys.rightlife.apimodel.appconfig
 
 data class AppConfigResponse(
-        val success: Boolean? = null,
-        val statusCode: Int? = null,
-        val data: Data? = null
+    val success: Boolean? = null,
+    val statusCode: Int? = null,
+    val data: Data? = null
 ) {
     data class Data(
-            val _id: String? = null,
-            val __v: Int? = null,
-            val appVersion: AppVersion? = null,
-            val createdAt: String? = null,
-            val forceUpdate: ForceUpdate? = null,
-            val imageUrl: ImageUrl? = null,
-            val maintenance: Maintenance? = null,
-            val razorpay: Razorpay? = null,
-            val signInWithEmail: Boolean? = null,
-            val signInWithMobile: Boolean? = null,
-            val updatedAt: String? = null
+        val _id: String? = null,
+        val __v: Int? = null,
+        val appVersion: AppVersion? = null,
+        val createdAt: String? = null,
+        val forceUpdate: ForceUpdate? = null,
+        val imageUrl: ImageUrl? = null,
+        val maintenance: Maintenance? = null,
+        val razorpay: Razorpay? = null,
+        val signInWithEmail: Boolean? = null,
+        val signInWithMobile: Boolean? = null,
+        val updatedAt: String? = null,
+        val isChallengeStart: Boolean = false
     )
 
     data class AppVersion(
-            val android: PlatformVersion? = null,
-            val ios: PlatformVersion? = null
+        val android: PlatformVersion? = null,
+        val ios: PlatformVersion? = null
     )
 
     data class PlatformVersion(
-            val min: String? = null,
-            val latest: String? = null
+        val min: String? = null,
+        val latest: String? = null
     )
 
     data class ForceUpdate(
-            val enabled: Boolean? = null,
-            val minIOSVersion: String? = null,
-            val latestIOSVersion: String? = null,
-            val updateIOSUrl: String? = null,
-            val minAndroidVersion: String? = null,
-            val latestAndroidVersion: String? = null,
-            val updateAndroidUrl: String? = null,
-            val message: String? = null
+        val enabled: Boolean? = null,
+        val minIOSVersion: String? = null,
+        val latestIOSVersion: String? = null,
+        val updateIOSUrl: String? = null,
+        val minAndroidVersion: String? = null,
+        val latestAndroidVersion: String? = null,
+        val updateAndroidUrl: String? = null,
+        val message: String? = null
     )
 
     data class ImageUrl(
-            val baseUrl: String? = null,
-            val cdnUrl: String? = null
+        val baseUrl: String? = null,
+        val cdnUrl: String? = null
     )
 
     data class Maintenance(
-            val enabled: Boolean? = null,
-            val message: String? = null
+        val enabled: Boolean? = null,
+        val message: String? = null
     )
 
     data class Razorpay(
-            val enabled: Boolean? = null,
-            val environment: String? = null,
-            val keyId: String? = null
+        val enabled: Boolean? = null,
+        val environment: String? = null,
+        val keyId: String? = null
     )
 }
